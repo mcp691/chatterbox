c65c904afa4a27fa192af9f3b4004a62b81dbce4
Matt Primpas <mattprimpas@bldup.com>
2018-12-11 20:19:25 -0500

    Upgrading to heroku-18

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,8 +87,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = ['https://chatterbocks.herokuapp.com',
-                                                 'http://chatterbocks.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://chatterbocks.herokuapp.com']
 
   config.web_socket_server_url = "wss://chatterbocks.herokuapp.com/cable"
 end
